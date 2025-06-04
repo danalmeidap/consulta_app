9869ea1a85a1f21a4a61e62b10b46150c2334d24
danalmeidap <dbatistadealmeida23@gmail.com>
2025-06-03 23:40:27 -0300

    feat: adiciona endpoint hello world e teste inicial

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from consulta_app.app import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get('/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'message': 'Welcome to Consulta App!'}
