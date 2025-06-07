b900df973cd5800a7cae7df8706054bc99f5d268
danalmeidap <dbatistadealmeida23@gmail.com>
2025-06-07 09:27:24 -0300

    feat(usuario) injestao de dependencias

--- consulta_app/repositories/usuario_repository.py
+++ consulta_app/repositories/usuario_repository.py
@@ -2,49 +2,42 @@ from typing import List, Optional
 
 from sqlmodel import Session, select
 
-from consulta_app.db.engine import engine
 from consulta_app.models.usuario import Usuario
 from consulta_app.schemas.usuario import UsuarioCreate
 
 
-def criar_usuario(usuario: UsuarioCreate) -> Usuario:
-    novo_usuario = Usuario(**usuario.model.dump())
-    with Session(engine) as session:
-        session.add(novo_usuario)
-        session.commit()
-        session.refresh(novo_usuario)
-        return novo_usuario
+def criar_usuario(usuario: UsuarioCreate, session: Session) -> Usuario:
+    novo_usuario = Usuario(**usuario.obj.model.dump())
+    session.add(novo_usuario)
+    session.commit()
+    session.refresh(novo_usuario)
+    return novo_usuario
 
 
-def listar_usuarios() -> List[Usuario]:
-    with Session(engine) as session:
-        usuarios = session.exec(select(Usuario)).all()
-        return usuarios
+def listar_usuarios(session: Session) -> List[Usuario]:
+    return session.exec(select(Usuario)).all()
 
 
-def buscar_usuario_por_id(usuario_id: int) -> Optional[Usuario]:
-    with Session(engine) as session:
-        return session.get(Usuario, usuario_id)
+def buscar_usuario_por_id(usuario_id: int, session: Session) -> Optional[Usuario]:  # noqa: E501
+    return session.get(Usuario, usuario_id)
 
 
-def atualizar_usuario(usuario_id: int, dados: UsuarioCreate) -> Optional[Usuario]:  # noqa: E501
-    with Session(engine) as session:
-        usuario = session.get(Usuario, usuario_id)
-        if not usuario:
-            return None
-        for key, value in dados.obj.model.dump().items():
-            setattr(usuario, key, value)
-        session.add(usuario)
-        session.commit()
-        session.refresh(usuario)
-        return usuario
+def atualizar_usuario(usuario_id: int, dados: UsuarioCreate, session: Session) -> Optional[Usuario]:  # noqa: E501
+    usuario = session.get(Usuario, usuario_id)
+    if not usuario:
+        return None
+    for key, value in dados.model.dump().items():
+        setattr(usuario, key, value)
+    session.add(usuario)
+    session.commit()
+    session.refresh(usuario)
+    return usuario
 
 
-def deletar_usuario(usuario_id: int) -> bool:
-    with Session(engine) as session:
-        usuario = session.get(Usuario, usuario_id)
-        if not usuario:
-            return False
-        session.delete(usuario)
-        session.commit()
-        return True
+def deletar_usuario(usuario_id: int, session: Session) -> bool:
+    usuario = session.get(Usuario, usuario_id)
+    if not usuario:
+        return False
+    session.delete(usuario)
+    session.commit()
+    return True
--- consulta_app/routes/usuarios.py
+++ consulta_app/routes/usuarios.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
 
+from consulta_app.db.dependencies import get_session
 from consulta_app.repositories import usuario_repository as repo
 from consulta_app.schemas.usuario import UsuarioCreate, UsuarioRead
 
@@ -7,34 +9,34 @@ router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
 
 @router.post("/", response_model=UsuarioRead)
-def criar(usuario: UsuarioCreate):
-    return repo.criar_usuario(usuario)
+def criar(usuario: UsuarioCreate, session: Session = Depends(get_session)):
+    return repo.criar_usuario(usuario, session)
 
 
 @router.get("/", response_model=list[UsuarioRead])
-def listar():
-    return repo.listar_usuarios()
+def listar(session: Session = Depends(get_session)):
+    return repo.listar_usuarios(session)
 
 
 @router.get("/{usuario_id}", response_model=UsuarioRead)
-def buscar(usuario_id: int):
-    usuario = repo.buscar_usuario_por_id(usuario_id)
+def buscar(usuario_id: int, session: Session = Depends(get_session)):
+    usuario = repo.buscar_usuario_por_id(usuario_id, session)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return usuario
 
 
 @router.put("/{usuario_id}", response_model=UsuarioRead)
-def atualizar(usuario_id: int, dados: UsuarioCreate):
-    usuario = repo.atualizar_usuario(usuario_id, dados)
+def atualizar(usuario_id: int, dados: UsuarioCreate, session: Session = Depends(get_session)):  # noqa: E501
+    usuario = repo.atualizar_usuario(usuario_id, dados, session)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return usuario
 
 
 @router.delete("/{usuario_id}")
-def deletar(usuario_id: int):
-    sucesso = repo.deletar_usuario(usuario_id)
+def deletar(usuario_id: int, session: Session = Depends(get_session)):
+    sucesso = repo.deletar_usuario(usuario_id, session)
     if not sucesso:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return {"mensagem": "Usuário removido com sucesso"}
